eb0899056f54a7d1a51ea16a7b2f35bd2bddbeac
FalconerOne <omajonathan@gmail.com>
2025-10-11 14:29:51 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,14 @@
-import type { AppProps } from "next/app";
-import "@/styles/globals.css";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      {/* Global sticky header */}
+    <main className="min-h-screen flex flex-col bg-gray-50">
       <Header />
-
-      {/* Add padding so page content doesn't hide under the fixed header */}
-      <div className="pt-20 min-h-screen bg-gray-50">
-        <main className="max-w-6xl mx-auto px-4">
-          <Component {...pageProps} />
-        </main>
+      <div className="flex-grow">
+        <Component {...pageProps} />
       </div>
-    </>
+      <Footer />
+    </main>
   );
 }
