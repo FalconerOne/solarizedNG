f481f70bd0c02363f81b47ea3d0bfc9b16bf7b8d
FalconerOne <omajonathan@gmail.com>
2025-10-12 12:41:40 +0100

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,5 @@
+<meta name="description" content="SolarizedNG Giveaway – Win, Share, Support & Impact lives!" />
+<meta property="og:image" content="/images/HHSF-2.png" />
+<meta property="og:title" content="SolarizedNG Charity Giveaway" />
+<meta property="og:url" content="https://solarizedng.vercel.app" />
+<meta name="twitter:card" content="summary_large_image" />
