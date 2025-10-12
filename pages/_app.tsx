13615bd7f82d4b83ed767e56b05f7fbf6987ac25
FalconerOne <omajonathan@gmail.com>
2025-10-12 09:06:31 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import type { AppProps } from "next/app";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import FloatingShareBar from "@/components/FloatingShareBar"; // ✅ Added import
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className="min-h-screen flex flex-col bg-gray-50">
+    <main className="min-h-screen flex flex-col bg-gray-50 font-[Segoe_UI]">
       <Header />
       <div className="flex-grow">
         <Component {...pageProps} />
       </div>
       <Footer />
+      <FloatingShareBar /> {/* ✅ Floating share bar appears site-wide, bottom-right */}
     </main>
   );
 }
