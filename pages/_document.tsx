f244d089aaf7eabe9bfa33ac8be3d654126b753d
FalconerOne <omajonathan@gmail.com>
2025-10-12 15:53:37 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,1 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#f97316" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="description" content="Win, Share, Support & Impact with SolarizedNG Charity Giveaway." />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
 
