4933750f4d919509dc6b907ebdb08978f0fe86d3
FalconerOne <omajonathan@gmail.com>
2025-10-12 11:22:14 +0100

    Create socials.ts

---
+++ lib/config/socials.ts
@@ -0,0 +1,29 @@
+// ✅ /config/socials.ts
+
+export const socialLinks = [
+  {
+    name: "Facebook",
+    icon: "FaFacebookF",
+    url: "https://facebook.com/solarizedng",
+  },
+  {
+    name: "X (Twitter)",
+    icon: "FaXTwitter",
+    url: "https://x.com/solarizedng",
+  },
+  {
+    name: "Instagram",
+    icon: "FaInstagram",
+    url: "https://instagram.com/solarizedng",
+  },
+  {
+    name: "LinkedIn",
+    icon: "FaLinkedinIn",
+    url: "https://linkedin.com/company/solarizedng",
+  },
+  {
+    name: "Telegram",
+    icon: "FaTelegramPlane",
+    url: "https://t.me/solarizedng",
+  },
+];
