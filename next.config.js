7751579eda9929cca051435e2fff11f72035bb79
FalconerOne <omajonathan@gmail.com>
2025-10-12 16:40:49 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
-  skipWaiting: true
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'cdn.jsdelivr.net']
-  },
-  swcMinify: true
 });
-
