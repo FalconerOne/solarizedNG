a2eb21f86bb3b32b4c8be3704b4e2bff3da89fe7
FalconerOne <omajonathan@gmail.com>
2025-10-12 14:27:45 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,6 +48,22 @@ export default function App({ Component, pageProps }: AppProps) {
           `,
         }}
       />
+      {/* Google Analytics */}
+<script
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-497701426"
+/>
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-XXXXXXXXXX');
+    `,
+  }}
+/>
+
     </main>
   );
 }
