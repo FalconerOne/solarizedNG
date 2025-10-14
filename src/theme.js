ea0ca686bf9eedfe1775cfe2e50803a0ad46f965
FalconerOne <omajonathan@gmail.com>
2025-10-14 10:11:18 +0100

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  brand: {
+    primary: "text-blue-600",
+    hover: "hover:text-blue-700",
+  },
+  bg: {
+    main: "bg-gray-50",
+    content: "bg-gray-100",
+  },
+  border: "border-gray-200",
+};
