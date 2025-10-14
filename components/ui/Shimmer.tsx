7395c2b8f590ab406cf4f18f60c0bdea2d1c828a
FalconerOne <omajonathan@gmail.com>
2025-10-14 20:51:29 +0100

    Create Shimmer.tsx

---
+++ components/ui/Shimmer.tsx
@@ -0,0 +1,6 @@
+"use client";
+export default function Shimmer() {
+  return (
+    <div className="w-full h-6 bg-gradient-to-r from-gray-700 via-gray-600 to-gray-700 animate-pulse rounded-md" />
+  );
+}
